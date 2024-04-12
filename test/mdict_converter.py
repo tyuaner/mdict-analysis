9e040aad3fda1ddb79911b11fe582abee32dbb8e
hurennian <hurennian@chilwee.com>
2024-04-12 16:37:42 +0800

    删除读取数据的方法，这里不需要

--- test/mdict_converter.py
+++ test/mdict_converter.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-import zlib
 import csv
 import os
-import re
+import zlib
+
 from readmdict import MDX, MDD
 
 
@@ -39,41 +39,3 @@ def generate_indexed_data(mdict_path):
 
             # 更新偏移量
             offset += length
-
-
-def retrieve_data_by_keys(keys, index_path):
-    """
-    根据提供的键列表从索引文件和数据文件中检索对应的数据，并处理重复键。
-    :param keys: 需要检索的键列表。
-    :param index_path: 索引文件的路径。
-    :return: 字典，键为索引中的键，值为一个列表，包含所有对应的值。
-    """
-    # 确定数据文件的路径
-    data_path = index_path.replace("_index.csv", "_data.bin")
-
-    # 读取索引文件
-    index_data = []
-    with open(index_path, 'r') as idx_file:
-        csv_reader = csv.reader(idx_file)
-        for row in csv_reader:
-            if row[0] in keys:
-                index_data.append((row[0], int(row[1]), int(row[2])))  # (key, offset, length)
-
-    # 按照索引中的 offset 进行排序
-    index_data.sort(key=lambda x: x[1])
-
-    # 读取数据文件并提取值
-    results = {}
-    with open(data_path, 'rb') as data_file:
-        for item in index_data:
-            key, offset, length = item
-            data_file.seek(offset)
-            mdict_value = data_file.read(length)
-            value = zlib.decompress(mdict_value).decode('utf-8') if re.search(r'_mdx_', data_path) else mdict_value
-
-            if key not in results:
-                results[key] = [value]
-            else:
-                results[key].append(value)
-
-    return results
