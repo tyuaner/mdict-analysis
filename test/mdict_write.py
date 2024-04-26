909578f40f5e987397e46647decba53310df82fa
hurennian <hurennian@chilwee.com>
2024-04-26 08:37:32 +0800

    oald10_converter.py

--- test/mdict_write.py
+++ test/mdict_write.py
@@ -2,5 +2,5 @@
 
 from mdict_converter import generate_indexed_data
 
-# generate_indexed_data('/Users/tyuaner/Local/mdict/MW11sound.mdx')
-# generate_indexed_data('/Users/tyuaner/Local/mdict/MW11sound.mdd')
+generate_indexed_data('/Users/tyuaner/Local/mdict/牛津高阶英汉双解词典（第10版）V3/牛津高阶英汉双解词典（第10版）V3.mdx')
+generate_indexed_data('/Users/tyuaner/Local/mdict/牛津高阶英汉双解词典（第10版）V3/牛津高阶英汉双解词典（第10版）V3.mdd')
