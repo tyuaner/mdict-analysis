0ea0bbc697198c9f4a9aa5e0f3206fdc05f4848a
hurennian <hurennian@chilwee.com>
2024-05-30 08:30:15 +0800

    转换朗文词典

--- test/mdict_write.py
+++ test/mdict_write.py
@@ -2,5 +2,5 @@
 
 from mdict_converter import generate_indexed_data
 
-generate_indexed_data('/Users/tyuaner/Local/mdict/牛津高阶英汉双解词典（第10版）V3/牛津高阶英汉双解词典（第10版）V3.mdx')
-generate_indexed_data('/Users/tyuaner/Local/mdict/牛津高阶英汉双解词典（第10版）V3/牛津高阶英汉双解词典（第10版）V3.mdd')
+generate_indexed_data('/Users/tyuaner/Local/mdict/LDOCE5/LDOCE5++ V 2-15.mdx')
+generate_indexed_data('/Users/tyuaner/Local/mdict/LDOCE5/LDOCE5++ V 2-15.mdd')
